38d7b23b2ad2c6c93d319fe857172c96b4c53cba
Petter Urkedal <paurkedal@gmail.com>
2017-05-28 17:24:46 +0200

    Switch to jbuilder.

--- pkg/pkg.ml
+++ pkg/pkg.ml
@@ -1,20 +1,8 @@
-#! /usr/bin/env ocaml
 #use "topfind"
-#require "topkg"
+#require "topkg-jbuilder"
+
 open Topkg
 
-let licenses = List.map Pkg.std_file ["COPYING.LESSER"; "COPYING"]
+let licenses = List.map Pkg.std_file ["COPYING"; "COPYING.LESSER"]
 
-(* Linting of META disabled due to presumed spurious complaints about the
- * ppx_driver predicate in the plugin assignments. *)
-let metas = [Pkg.meta_file ~lint:false "pkg/META"]
-
-let () = Pkg.describe ~licenses ~metas "ppx_compose" @@ fun c ->
-  Ok [
-    Pkg.lib "ppx_compose.cmo";
-    Pkg.lib "ppx_compose.cmx";
-    Pkg.lib "ppx_compose.o";
-    Pkg.libexec ~dst:"ppx_compose" "ppx_compose_main";
-    Pkg.mllib "ppx_compose_runtime.mllib";
-    Pkg.test "test_compose";
-  ]
+let () = Topkg_jbuilder.describe ~licenses ()
